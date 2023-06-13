617d5b8d15ed3037f596e7a19addf3e30a102145
Mahsa <mahsa.1d.1377@gmail.com>
2023-06-13 11:08:24 +0330

    added Roles migration and updated User. Everything Woks 😪

--- database/migrations/1686570065155_users.ts
+++ database/migrations/1686570065155_users.ts
@@ -1,22 +1,25 @@
-import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+// import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 
-export default class extends BaseSchema {
-  protected tableName = 'users'
+// export default class extends BaseSchema {
+//   protected tableName = 'users'
 
-  public async up () {
-    this.schema.createTable(this.tableName, (table) => {
-      table.increments('id').primary()
-      table.string('name').notNullable()
-      table.string('email').notNullable().unique()
-      table.string('password').notNullable()
-      table.enum('role', ['admin' , 'user']).notNullable().defaultTo('user')
-      table.string('photo_path')
-      table.timestamp('created_at', { useTz: true })
-      table.timestamp('updated_at', { useTz: true })
-    })
-  }
+//   public async up () {
+//     this.schema.createTable(this.tableName, (table) => {
+//       table.increments('id').primary()
+//       table.string('name').notNullable()
+//       table.string('email', 255).notNullable().unique()
+//       table.string('password', 180).notNullable()
+//       table.string('remember_me_token').nullable()
+//       table.integer('role_id').unsigned().references('id').inTable('roles').defaultTo(1)
+//       //table.enum('role', ['admin', 'user']).notNullable().defaultTo('user')
+//       table.string('photo_path')
+//       table.timestamp('created_at', { useTz: true })
+//       table.timestamp('updated_at', { useTz: true })
+//     })
+//   }
 
-  public async down () {
-    this.schema.dropTable(this.tableName)
-  }
-}
+//   public async down () {
+//     this.schema.dropTable(this.tableName)
+//   }
+// }
+  
